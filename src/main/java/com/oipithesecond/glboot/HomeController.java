dec376fce8980afeb3f390b1652d5912e26b2ee6
oipi <tuberosumgames@gmail.com>
2025-06-30 23:32:21 +0530

    controllers

---
+++ src/main/java/com/oipithesecond/glboot/HomeController.java
@@ -0,0 +1,12 @@
+package com.oipithesecond.glboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
