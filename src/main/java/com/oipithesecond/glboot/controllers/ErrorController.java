db5258d98bc50432ff5ab95ff0e96ca689885aac
oipi <tuberosumgames@gmail.com>
2025-07-18 20:02:32 +0530

    error controllers

---
+++ src/main/java/com/oipithesecond/glboot/controllers/ErrorController.java
@@ -0,0 +1,33 @@
+package com.oipithesecond.glboot.controllers;
+
+import com.oipithesecond.glboot.domain.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
+@Slf4j
+public class ErrorController {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex){
+        log.error("caught exception", ex);
+        ErrorResponse error = ErrorResponse.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("an unexpected error occurred")
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgsException(IllegalArgumentException ex){
+        ErrorResponse error = ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
--- src/main/java/com/oipithesecond/glboot/domain/dto/ErrorResponse.java
+++ src/main/java/com/oipithesecond/glboot/domain/dto/ErrorResponse.java
@@ -1,8 +1,28 @@
 package com.oipithesecond.glboot.domain.dto;
 
-public record ErrorResponse(
-        int status,
-        String message,
-        String details
-) {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse{
+    private int status;
+    private String message;
+    private List<FieldError> fieldErrors;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class FieldError{
+        private String field;
+        private String message;
+    }
 }
+//
